2fb4bcbca00a6d92ea960d0329b5a87e06ad661c
Hudzaifah1610 <Hudzaifah1610@gmail.com>
2019-10-28 17:22:21 +0700

    newosans

--- src/components/LandingPage/Contact/Contact.jsx
+++ src/components/LandingPage/Contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="row ">
-        <div className="col">
+        <div className="col all-cont">
           <div className="row m-auto">
             <p>Contacts</p>
           </div>
@@ -13,8 +13,8 @@ const Contact = () => {
             <div className="col-lg-4">
               <div className="icon-contact">
                 <i className="fa fa-map-marker"></i>
-                <h6>60 Paya Lebar Ro 45</h6>
-                <h6>60 Paya Lebar Roa 45, Indonesian</h6>
+                <h6>Pondok Programmer</h6>
+                <h6>Kretek,Bantul,DIY</h6>
               </div>
             </div>
             <div className="col-lg-4">
@@ -29,8 +29,8 @@ const Contact = () => {
               <div className="icon-contact">
                 <i className="fa fa-envelope"></i>
                 {/* <h6>60 Paya Lebar Roa SG, Singapore</h6> */}
-                <h6>Yourmail@gmail.com</h6>
-                <h6>backpiper.com@gmail.com</h6>
+                <h6>OSans@gmail.com</h6>
+                <h6>Projekmini@gmail.com</h6>
               </div>
             </div>
           </div>
